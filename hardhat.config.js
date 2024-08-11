ae42f3d5192b45e22c33f96b9a32d9dd2ddce958
aritrag281 <aritrag281@gmail.com>
2024-08-11 12:15:41 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
     networks:{
       sepolia: {
       url: `https://sepolia.infura.io/v3/008163aea6bf4cce9b12f215ec5d09fd`,
-      accounts: [`0xc2c4ddc5a8d9954162c8afc414f0102a9d35d4553bfc3a99b7c2b38437d498fa`], // Make sure to replace with your private key
+      accounts: [`yourprivatekey`], // Make sure to replace with your private key
       chainId: 11155111,
     },
   },
